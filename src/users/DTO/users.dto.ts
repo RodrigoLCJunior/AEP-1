b8e0c4e255afd30c4a7ce61c9438ac5e60fad648
RodrigoLCJunior <165792809+RodrigoLCJunior@users.noreply.github.com>
2024-10-23 19:37:39 -0300

    Update users.dto.ts

--- src/users/DTO/users.dto.ts
+++ src/users/DTO/users.dto.ts
@@ -1,7 +1,12 @@
 import { Posts } from "src/posts/posts.entity";
 
 export class UsersDTO {
+
+    //@inNotEmpty
     name: string
+
+    //@inNotEmpty
+    //@
     email: string
     senha: string
     isActive: boolean
